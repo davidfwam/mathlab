89afd73eea44eae430d7fc5c311a81a3daf2eef3
dbehailu <dagmbehailu@gmail.com>
2017-07-25 15:14:08 -0400

    added addition and subtraction functions

---
+++ functions/MultiplicationHard/MultiplicationHard.js
@@ -0,0 +1,24 @@
+var numberone = Math.floor(Math.random() * 1001);
+var numbertwo = Math.floor(Math.random() * 1001);
+var n1 = numberone.toString();
+var n2 = numbertwo.toString();
+function MultiplicationHard(){
+var user_sum = $('#user_input').val();
+var sumofrandint = numberone * numbertwo;
+var usersum = Number(user_sum);
+
+if (sumofrandint == usersum){
+    $(".response").append("<p> Congrats! Your answer is correct. </p>");
+}
+else{
+    var finalsum = sumofrandint.toString();
+    $(".response").append("<p> Sorry. The correct answer is " + finalsum + ".</p>");
+}
+}
+
+function setup() {
+  $(".function").append("<p>" + n1 + "*" + n2 + "= </p>");
+  $("#submit_button").click(MultiplicationHard);
+ }
+
+$(document).ready(setup)
